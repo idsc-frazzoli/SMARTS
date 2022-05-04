ca0936fa755876fcb99e7938f90db3ab8c58c380
nkaslin <kaeslin.nando@gmail.com>
2022-05-04 17:27:43 +0200

    alpha2

--- baselines/marl_benchmark/run_experiments.sh
+++ baselines/marl_benchmark/run_experiments.sh
@@ -200,19 +200,19 @@
 #  python run.py scenarios/custom/merge/merge65_lanes42 -f marl_benchmark/agents/ppo/merge65_lanes42_alpha/baseline-lane-control_cent_01_2.yaml --num_workers 5 --headless --paradigm centralized --stop_time 150000 --horizon 80 --log_dir ./log/results/run/20220504_merge65_lanes42/alpha01_degree2/cent
 #done
 
-## 5 runs decent and cent with merge65_lanes42 symmetric cost 07, 13k episodes per run, alpha = 2, degree = 2
-#scl scenario build-all scenarios/custom/merge/merge65_lanes42
-#for (( i=0; i<5; i++ ))
-#do
-#  python run.py scenarios/custom/merge/merge65_lanes42 -f marl_benchmark/agents/ppo/merge65_lanes42_alpha/baseline-lane-control_decent_2_2.yaml --num_workers 5 --headless --paradigm decentralized --stop_time 150000 --horizon 80 --log_dir ./log/results/run/20220504_merge65_lanes42/alpha2_degree2/decent
-#  python run.py scenarios/custom/merge/merge65_lanes42 -f marl_benchmark/agents/ppo/merge65_lanes42_alpha/baseline-lane-control_cent_2_2.yaml --num_workers 5 --headless --paradigm centralized --stop_time 150000 --horizon 80 --log_dir ./log/results/run/20220504_merge65_lanes42/alpha2_degree2/cent
-#done
-#
-# 5 runs decent and cent with merge65_lanes42 symmetric cost 07, 13k episodes per run, alpha = 10, degree = 2
+# 5 runs decent and cent with merge65_lanes42 symmetric cost 07, 13k episodes per run, alpha = 2, degree = 2
 scl scenario build-all scenarios/custom/merge/merge65_lanes42
 for (( i=0; i<5; i++ ))
 do
-  python run.py scenarios/custom/merge/merge65_lanes42 -f marl_benchmark/agents/ppo/merge65_lanes42_alpha/baseline-lane-control_decent_10_2.yaml --num_workers 5 --headless --paradigm decentralized --stop_time 150000 --horizon 80 --log_dir ./log/results/run/20220504_merge65_lanes42/alpha10_degree2/decent
-  python run.py scenarios/custom/merge/merge65_lanes42 -f marl_benchmark/agents/ppo/merge65_lanes42_alpha/baseline-lane-control_cent_10_2.yaml --num_workers 5 --headless --paradigm centralized --stop_time 150000 --horizon 80 --log_dir ./log/results/run/20220504_merge65_lanes42/alpha10_degree2/cent
+  python run.py scenarios/custom/merge/merge65_lanes42 -f marl_benchmark/agents/ppo/merge65_lanes42_alpha/baseline-lane-control_decent_2_2.yaml --num_workers 5 --headless --paradigm decentralized --stop_time 150000 --horizon 80 --log_dir ./log/results/run/20220504_merge65_lanes42/alpha2_degree2/decent
+  python run.py scenarios/custom/merge/merge65_lanes42 -f marl_benchmark/agents/ppo/merge65_lanes42_alpha/baseline-lane-control_cent_2_2.yaml --num_workers 5 --headless --paradigm centralized --stop_time 150000 --horizon 80 --log_dir ./log/results/run/20220504_merge65_lanes42/alpha2_degree2/cent
 done
+
+## 5 runs decent and cent with merge65_lanes42 symmetric cost 07, 13k episodes per run, alpha = 10, degree = 2
+#scl scenario build-all scenarios/custom/merge/merge65_lanes42
+#for (( i=0; i<5; i++ ))
+#do
+#  python run.py scenarios/custom/merge/merge65_lanes42 -f marl_benchmark/agents/ppo/merge65_lanes42_alpha/baseline-lane-control_decent_10_2.yaml --num_workers 5 --headless --paradigm decentralized --stop_time 150000 --horizon 80 --log_dir ./log/results/run/20220504_merge65_lanes42/alpha10_degree2/decent
+#  python run.py scenarios/custom/merge/merge65_lanes42 -f marl_benchmark/agents/ppo/merge65_lanes42_alpha/baseline-lane-control_cent_10_2.yaml --num_workers 5 --headless --paradigm centralized --stop_time 150000 --horizon 80 --log_dir ./log/results/run/20220504_merge65_lanes42/alpha10_degree2/cent
+#done
 
